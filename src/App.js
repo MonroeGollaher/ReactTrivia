e2e5afba349f18f797d9af4d75bdb27b3f37e3bb
Monroe Gollaher <mgollaher92@gmail.com>
2021-04-13 22:02:09 -0600

    question & answers retrived from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import { getQuestions } from "./services/triviaService";
+import Card from "./services/components/Card/Card";
 
 function App() {
+
+  const [questionData, setQuestionData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const initUrl = 'https://opentdb.com/api.php?amount=10';
+
+  useEffect(() => {
+    async function fetchData(){
+      let res = await getQuestions(initUrl)
+      await loadQuestions(res.results)
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
+
+  const loadQuestions = async (data) => {
+    let _questionData = data
+    console.log(data);
+    setQuestionData(_questionData)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      { questionData.map((question, index) => {
+        return (
+          <Card
+            key={index }
+            question={ question.question }
+            correct_answer={ question.correct_answer }
+          /> 
+        )
+        })
+      }
     </div>
   );
 }
